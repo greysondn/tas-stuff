dc602e32fcf1987891d8ef9ac2e81890f2716aae
J. 'Dorian Greyson' L <greysondn@gmail.com>
2021-04-05 01:23:00 -0400

    greatly improve testing and coverage reporting

--- python/runtests.bat
+++ python/runtests.bat
@@ -1,9 +1,26 @@
 @echo off
-nosetests                               ^
-                                        ^
-    --with-coverage                     ^
-    --all-modules                       ^
-    --cover-html                        ^
-    --cover-html-dir=coverage           ^
-    --cover-erase                       ^
-    
+REM This assumes that nose is inside the Python 37 location.
+REM Linux try $(where nosetests)
+REM Windows, good luck, maybe start with $: where nosetests
+
+REM destroy coverage data so it's built ground up
+del .coverage
+
+coverage                                                                       ^
+    run                                                                        ^
+--omit="./tasstuff/test/*","*/__init__.py"                                     ^
+        "C:\Program Files\Python37\lib\site-packages\nose\core.py"             ^
+            --all-modules                                                      ^
+            .                                                                  ^
+            
+
+REM issue coverage report at command line
+echo.
+coverage report
+
+REM destroy coverage html so it's built ground up
+rmdir /Q /S coverage
+
+coverage                                                                       ^
+    html                                                                       ^
+        --directory=coverage                                                   ^
