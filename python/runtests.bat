cc4dccf4bc4c27cd843a91a10d3e274f5bb9de2f
J. 'Dorian Greyson' L <greysondn@gmail.com>
2021-04-04 20:51:18 -0400

    testing - write batch file

---
+++ python/runtests.bat
@@ -0,0 +1,9 @@
+@echo off
+nosetests                               ^
+                                        ^
+    --with-coverage                     ^
+    --all-modules                       ^
+    --cover-html                        ^
+    --cover-html-dir=coverage           ^
+    --cover-erase                       ^
+    
