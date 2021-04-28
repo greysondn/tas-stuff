61722b4ba138993da08339a985ae45eb15ccab22
J. 'Dorian Greyson' L <greysondn@gmail.com>
2021-04-28 19:19:48 -0400

    Start work on the Piano Roll. Not sure where I'm going with this.

--- python/tasstuff/any/bizhawk/history.py
+++ python/tasstuff/any/bizhawk/history.py
@@ -1,6 +1,16 @@
 class PianoRollEntry():
     # at the end of the day, this is just a frame and a set of inputs
-    def __init__(self, input="", frame=-1):
-        self.input = input
+    def __init__(self, inpt="", frame=-1):
+        self.input = inpt
         self.frame = frame
-        
+
+class PianoRoll():
+    def __init__(self):
+        # this is a little more complex, I think
+        self.frames = []
+        self.currentFrame = 0
+    
+    def addFrame(self, inpt):
+        swp = PianoRollEntry(inpt, self.currentFrame)
+        self.frames.append(swp)
+        self.currentFrame = self.currentFrame + 1
--- python/tasstuff/test/any/bizhawk/history.py
+++ python/tasstuff/test/any/bizhawk/history.py
@@ -2,7 +2,7 @@ import unittest
 
 import tasstuff.any.bizhawk.history as history
 
-class TestPianoRollEntry(unittest.TestCase):
+class Test_PianoRollEntry(unittest.TestCase):
     def test_constructor_default(self):
         tst = history.PianoRollEntry()
         
@@ -10,7 +10,23 @@ class TestPianoRollEntry(unittest.TestCase):
         self.assertEqual(tst.frame, -1)
 
     def test_constructor_args(self):
-        tst = history.PianoRollEntry(input="test input", frame=42)
+        tst = history.PianoRollEntry(inpt="test input", frame=42)
 
         self.assertEqual(tst.input, "test input")
         self.assertEqual(tst.frame, 42)
+
+class Test_PianoRoll(unittest.TestCase):
+    def test_constructor_default(self):
+        tst = history.PianoRoll()
+        
+        self.assertEqual(len(tst.frames), 0)
+        self.assertEqual(tst.currentFrame, 0)
+    
+    def test_addFrame(self):
+        tst = history.PianoRoll()
+
+        tst.addFrame("|test|input|here|")
+
+        self.assertEqual(len(tst.frames), 1)
+        self.assertEqual(tst.currentFrame, 1)
+        self.assertEqual(tst.frames[0].input, "|test|input|here|")
